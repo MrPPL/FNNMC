d1dd806cd59b23fa9451bf762921aac8e2b4d4b4
Peter Pommergård Lind <ppl_peter@live.dk>
2021-05-08 19:57:22 +0200

    update .gitignore file

---
+++ LSM.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
+
+print("Hello World!")
+
+
+print("HEllo World")
+print("hey there")
