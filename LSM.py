6653de06db165c4e9fe57fd743253fd28f89a411
Peter Lind <ppl_peter@live.dk>
2021-06-13 17:21:00 +0200

    unittest works again

--- LSM.py
+++ LSM.py
@@ -114,3 +114,31 @@ def priceAmericanOption(coefficientMatrix, simulatedPaths, Option, MarketVariabl
             simulatedPaths[:,timeStep][pathsITM] = cashFlowChoice[pathsITM]
 
     return simulatedPaths[:,1].mean()*np.exp(-MarketVariables.r*timeIncrement)
+
+##########################
+# Testing
+##########################
+if __name__ == '__main__':
+    #Price American Put
+    timeStepsTotal = 50
+    normalizeStrike=40
+    putOption = Option(strike=1,payoffType="Put", timeToMat=1)
+    for spot1 in range(36,46,2):
+        MarketVariablesEx1 = MarketVariables(r=0.06,vol=0.2, spot=spot1/normalizeStrike)
+        learningPaths= generateSDEStockPaths(pathTotal=10**5, timeStepsTotal=timeStepsTotal, timeToMat=putOption.timeToMat, MarketVariables=MarketVariablesEx1)
+        regressionCoefficient = findRegressionCoefficient(basisFuncTotal=5, Option=putOption, simulatedPaths=learningPaths, MarketVariables=MarketVariablesEx1)
+        pricingPaths= generateSDEStockPaths(pathTotal=10**5, timeStepsTotal=timeStepsTotal, timeToMat=putOption.timeToMat, MarketVariables=MarketVariablesEx1)
+        priceAmerPut = priceAmericanOption(coefficientMatrix=regressionCoefficient, Option=putOption , simulatedPaths=pricingPaths, MarketVariables=MarketVariablesEx1)*normalizeStrike
+        print("Spot: ", spot1, "Price American Put: ", priceAmerPut)
+        
+
+    #Price American call aka european call
+    timeStepsTotal = 50
+    normalizeStrike=40
+    putOption = Option(strike=1,payoffType="Call", timeToMat=1)
+    MarketVariablesEx1 = MarketVariables(r=0.06,vol=0.2, spot=1)
+    learningPaths= generateSDEStockPaths(pathTotal=10**5, timeStepsTotal=timeStepsTotal, timeToMat=putOption.timeToMat, MarketVariables=MarketVariablesEx1)
+    regressionCoefficient = findRegressionCoefficient(basisFuncTotal=2, Option=putOption, simulatedPaths=learningPaths, MarketVariables=MarketVariablesEx1)
+    pricingPaths= generateSDEStockPaths(pathTotal=10**4, timeStepsTotal=timeStepsTotal, timeToMat=putOption.timeToMat, MarketVariables=MarketVariablesEx1)
+    priceAmerCall = priceAmericanOption(coefficientMatrix=regressionCoefficient, Option=putOption , simulatedPaths=pricingPaths, MarketVariables=MarketVariablesEx1)*normalizeStrike
+    print("Spot: ", MarketVariablesEx1.spot*normalizeStrike, "American Call Price: ", priceAmerCall)
