8fd6f5d1e2c420696283179b1f74aa2a12c796ca
Peter Lind <ppl_peter@live.dk>
2021-06-27 20:31:47 +0200

    Updated with valueVec instead of matrix

--- LSMBasketOption.py
+++ LSMBasketOption.py
@@ -33,16 +33,15 @@ def findRegressionCoefficient(simulatedPaths, basisFuncTotal, Option, MarketVari
     timeStepsTotal = np.shape(simulatedPaths)[0]-1
     pathsTotal = np.shape(simulatedPaths)[1]
     coefficientMatrix = np.zeros((basisFuncTotal,timeStepsTotal))
-    ValueMatrix=np.zeros((pathsTotal, timeStepsTotal+1))
 
     timeIncrement = Option.timeToMat/(timeStepsTotal)
     for timeStep in range(timeStepsTotal,0,-1):
         #Get payoff at maturity
         if(timeStep== (timeStepsTotal) ):
-            ValueMatrix[:,timeStep] = Option.payoff(simulatedPaths[timeStep,:,:])
+            ValueVec = Option.payoff(simulatedPaths[timeStep,:,:])
         #Find regressionscoefficients at each exercise dates before maturity
         else:
-            response = np.exp(-MarketVariables.r*timeIncrement)*ValueMatrix[:,timeStep+1]
+            response = np.exp(-MarketVariables.r*timeIncrement)*ValueVec
             currentSpots = simulatedPaths[timeStep,:,:]
             covariates = generateDesignMatrix(currentSpots)
             pathsITM = np.where(Option.payoff(currentSpots)>0)
@@ -52,14 +51,14 @@ def findRegressionCoefficient(simulatedPaths, basisFuncTotal, Option, MarketVari
                 expectedContinuationValue = regressionFit.predict(covariates)
                 intrinsicValue = Option.payoff(currentSpots)
 
-                ValueMatrix[:,timeStep] = response
+                ValueVec = response
                 #overwrite the default to keep the option alive, if it is beneficial to keep the exercise for the ITM paths.
                 #CashFlow from decision wheather to stop or keep option alive
                 cashFlowChoice = np.where(intrinsicValue>expectedContinuationValue, intrinsicValue, response)
-                ValueMatrix[:,timeStep][pathsITM] = cashFlowChoice[pathsITM]
+                ValueVec[pathsITM] = cashFlowChoice[pathsITM]
             else:
                 coefficientMatrix[:,timeStep]= 0
-                ValueMatrix[:,timeStep] = response
+                ValueVec = response
 
     return coefficientMatrix
 
@@ -69,26 +68,25 @@ def findRegressionCoefficient(simulatedPaths, basisFuncTotal, Option, MarketVari
 def priceAmericanOption(coefficientMatrix, simulatedPaths, Option, MarketVariables):
     timeStepsTotal = simulatedPaths.shape[0]-1 #time 0 does not count to a timestep
     timeIncrement = Option.timeToMat/timeStepsTotal
-    ValueMatrix=np.zeros((simulatedPaths.shape[1], timeStepsTotal+1))
     for timeStep in range(timeStepsTotal,0,-1):
         #Get payoff at maturity
         if(timeStep==timeStepsTotal):
-            ValueMatrix[:,timeStep] = Option.payoff(simulatedPaths[timeStep,:,:])
+            ValueVec = Option.payoff(simulatedPaths[timeStep,:,:])
         #Use coefficientMatrix and paths to price american option 
         else:
-            continuationValue = np.exp(-MarketVariables.r*timeIncrement)*ValueMatrix[:,timeStep+1]
+            continuationValue = np.exp(-MarketVariables.r*timeIncrement)*ValueVec
             currentSpots = simulatedPaths[timeStep,:,:]
             covariates = generateDesignMatrix(currentSpots)
             expectedContinuationValue = np.matmul(covariates, coefficientMatrix[1:,timeStep])+ coefficientMatrix[0,timeStep]
             intrinsicValue = Option.payoff(currentSpots)
             #overwrite the default to keep the option alive, if it is beneficial to keep the exercise for the ITM paths.
-            ValueMatrix[:,timeStep] = continuationValue #default value to keep option alive
+            ValueVec = continuationValue #default value to keep option alive
             cashFlowChoice = np.where(intrinsicValue>expectedContinuationValue, intrinsicValue, continuationValue)
 
             pathsITM = np.where(intrinsicValue>0)
-            ValueMatrix[:,timeStep][pathsITM] = cashFlowChoice[pathsITM]
+            ValueVec[pathsITM] = cashFlowChoice[pathsITM]
 
-    return ValueMatrix[:,1].mean()*np.exp(-MarketVariables.r*timeIncrement)
+    return ValueVec.mean()*np.exp(-MarketVariables.r*timeIncrement)
 
 import Products
 import SimulationPaths.GBMMultiDim
@@ -106,7 +104,7 @@ if __name__ == '__main__':
     print(f"Time taken to simulate paths is: {timeSimPathsEnd-timeSimPathsStart:f}")
 
     timeRegressionStart = time.time()
-    coefMatrix = findRegressionCoefficient(simulatedPaths=learningPaths, basisFuncTotal=7, Option=callMax, MarketVariables=marketVariables)
+    coefMatrix = findRegressionCoefficient(simulatedPaths=learningPaths, basisFuncTotal=3, Option=callMax, MarketVariables=marketVariables)
     timeRegressionEnd = time.time()
     print(f"Time taken for find regressioncoefficients: {timeRegressionEnd-timeRegressionStart:f}")
     pricingPaths = SimulationPaths.GBMMultiDim.simulatePaths(timeStepsTotal=timeStepsTotal,pathsTotal=pathTotal, marketVariables=marketVariables, timeToMat=callMax.timeToMat)
--- test/test_LSMMultiDim.py
+++ test/test_LSMMultiDim.py
@@ -31,6 +31,34 @@ class Test_LSMMultiDim(unittest.TestCase):
                                         [0,-1.7],
                                         [0,-2.00]])
         self.assertEqual(np.allclose(actualRegressionCoef[:,1:], expectedRegressionCoef[:,1:], 0.01), True)
+    def testPricing(self):
+        """Use the regression example in LSM paper chapter 1
+        """
+        learningPaths = np.array([[[1., 1.],
+        [1., 1.],
+        [1., 1.],
+        [1., 1.],
+        [1., 1.]],
+
+       [[1.1, 0.9],
+        [1.0, 1.0],
+        [0.9, 0.9],
+        [0.8, 1.2],
+        [1.0, 1.05]],
+
+       [[1.20, 0.85],
+        [1.02, 0.93],
+        [0.85, 0.87],
+        [0.95, 1.11],
+        [1.05, 1.07]]])
+        marketVariablesEX = Products.MarketVariables(r=0.00,vol=0,spot=1)
+        callMaxOption = Products.Option(strike=1.0, typeOfContract="CallMax",timeToMat=2)
+
+        regressionCoef = np.array([[0,3.87], [0,-1.7], [0,-2]])
+
+        actualPrice = LSMBasketOption.priceAmericanOption(coefficientMatrix=regressionCoef, simulatedPaths=learningPaths, Option=callMaxOption, MarketVariables=marketVariablesEX)
+        expectedPrice = 0.098
+        self.assertAlmostEqual(first=actualPrice,second=expectedPrice,places=3)
 
 if __name__ == '__main__':
     unittest.main()
